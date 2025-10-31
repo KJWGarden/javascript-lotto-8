88f55addd04a91b07f0100599ee749975d79a85b
KJWGarden <mike03131313@gmail.com>
2025-10-31 22:45:30 +0900

    feat: 사용자에게 입력받기
    사용자에게 구입금액 입력받는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    this.getPurchase();
+  }
+
+  async getPurchase() {
+    const inputcost = await Console.readLineAsync("구입금액을 입력해 주세요");
+  }
 }
 
 export default App;

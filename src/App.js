4bc0d38f243b419579c3263fc978dc7116810ac5
KJWGarden <mike03131313@gmail.com>
2025-11-01 00:19:28 +0900

    feat: 구입금액 예외처리
    구입 금액이 1000원 단위가 아닐경우 예외처리 함수 구현

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    this.getPurchase();
+    const cost = await this.getPurchase();
+    this.costException(cost);
   }
 
   async getPurchase() {
     const inputcost = await Console.readLineAsync("구입금액을 입력해 주세요");
+    return inputcost;
+  }
+
+  costException(cost) {
+    if (cost % 1000 != 0) {
+      throw new Error("[ERROR] 구입금액은 1000원 단위로만 가능합니다.");
+    }
   }
 }
 

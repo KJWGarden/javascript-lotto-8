7e232d6a766e7a8268829a7dac1a20251273d4b6
KJWGarden <mike03131313@gmail.com>
2025-11-02 20:34:48 +0900

    docs: README.md 수정
    구입금액 예외처리 세부요구사항 추가

--- src/App.js
+++ src/App.js
@@ -102,9 +102,6 @@ class App {
     if (bonusNum == null || isNaN(bonusNum)) {
       throw new Error("[ERROR] 보너스 번호를 입력해야 합니다.");
     }
-    if (/[^0-9]/.test(bonusNum)) {
-      throw new Error("[ERROR] 보너스 번호는 숫자만 입력해야 합니다.");
-    }
     if (bonusNum < 1 || bonusNum > 45) {
       throw new Error("[ERROR] 보너스 번호는 1과 45 사이의 숫자여야 합니다.");
     }

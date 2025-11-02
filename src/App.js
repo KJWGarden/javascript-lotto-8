d7857f2b00124ddf6b238bfd078d7f140e469f8c
KJWGarden <mike03131313@gmail.com>
2025-11-02 20:11:05 +0900

    feat: 등수별 당첨 금액 계산
    등수별로 당첨 금액 계산 함수 구현

--- src/App.js
+++ src/App.js
@@ -125,6 +125,16 @@ class App {
     }
     return result;
   }
+
+  getPrizeMoney() {
+    return {
+      1: 2000000000,
+      2: 30000000,
+      3: 1500000,
+      4: 50000,
+      5: 5000,
+    };
+  }
 }
 
 export default App;

62e11202daf985745dad57a891a22295b2a644f7
KJWGarden <mike03131313@gmail.com>
2025-11-03 00:39:31 +0900

    test(LottoTest): 테스트 코드 추가
    로또 번호에 빈 값이 존재할 때 예외처리 테스트

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -20,4 +20,10 @@ describe("로또 클래스 테스트", () => {
       new Lotto([1, 2, 3, 4, 5, "@"]);
     }).toThrow("[ERROR]");
   });
+
+  test("로또 번호에 빈 값이 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, , 5, 6]);
+    }).toThrow("[ERROR]");
+  });
 });

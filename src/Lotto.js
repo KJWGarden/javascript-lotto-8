7e00a2c698a3234fe6eb152c14d7a6165030c02d
KJWGarden <mike03131313@gmail.com>
2025-11-03 00:29:33 +0900

    feat: 로또 클래스 검증 로직 추가
    로또 클래스에 validate 검증로직 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,8 +7,17 @@ class Lotto {
   }
 
   #validate(numbers) {
+    if (!Array.isArray(numbers)) {
+      throw new Error("[ERROR] 로또 번호는 배열이어야 합니다.");
+    }
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+    if (new Set(numbers).size !== numbers.length) {
+      throw new Error("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
+    }
+    if (!numbers.every((n) => n > 0 && n < 46)) {
+      throw new Error("[ERROR] 로또 번호는 1~45 사이여야 합니다.");
     }
   }
 
